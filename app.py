ac0c7ed43a078e1d10fa44d54efbbc450ddcd3a1
Bima <bima20022307@gmail.com>
2024-12-11 08:40:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,58 +33,61 @@ with st.sidebar:
 
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
+    tab1, tab2 = st.tabs(["**Prediksi Satuan**", "**Prediksi Tabel**"])
+    with tab1:
+      # page title
+      st.title('Diabetes Prediction using SVM Method')
+  
+      # getting the input data from the user
+      col1, col2, col3 = st.columns(3)
+  
+      with col1:
+          Pregnancies = st.text_input('Number of Pregnancies')
+  
+      with col2:
+          Glucose = st.text_input('Glucose Level')
+  
+      with col3:
+          BloodPressure = st.text_input('Blood Pressure value')
+  
+      with col1:
+          SkinThickness = st.text_input('Skin Thickness value')
+  
+      with col2:
+          Insulin = st.text_input('Insulin Level')
+  
+      with col3:
+          BMI = st.text_input('BMI value')
+  
+      with col1:
+          DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+  
+      with col2:
+          Age = st.text_input('Age of the Person')
+  
+  
+      # code for Prediction
+      diab_diagnosis = ''
+  
+      # creating a button for Prediction
+  
+      if st.button('Diabetes Test Result'):
+  
+          user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                        BMI, DiabetesPedigreeFunction, Age]
+  
+          user_input = [float(x) for x in user_input]
+  
+          diab_prediction = diabetes_model.predict([user_input])
+  
+          if diab_prediction[0] == 1:
+              diab_diagnosis = 'The person is diabetic'
+          else:
+              diab_diagnosis = 'The person is not diabetic'
+  
+      st.success(diab_diagnosis)
+    with tab2:  
 
-    # page title
-    st.title('Diabetes Prediction using SVM Method')
-
-    # getting the input data from the user
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
-
-    with col2:
-        Glucose = st.text_input('Glucose Level')
-
-    with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
-
-    with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
-
-    with col2:
-        Insulin = st.text_input('Insulin Level')
-
-    with col3:
-        BMI = st.text_input('BMI value')
-
-    with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-
-    with col2:
-        Age = st.text_input('Age of the Person')
-
-
-    # code for Prediction
-    diab_diagnosis = ''
-
-    # creating a button for Prediction
-
-    if st.button('Diabetes Test Result'):
-
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
-                      BMI, DiabetesPedigreeFunction, Age]
-
-        user_input = [float(x) for x in user_input]
-
-        diab_prediction = diabetes_model.predict([user_input])
-
-        if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
-        else:
-            diab_diagnosis = 'The person is not diabetic'
-
-    st.success(diab_diagnosis)
 
 # Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
